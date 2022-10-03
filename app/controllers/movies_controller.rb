6d3bb300857abe430ca3b7ebfbd4789858eb90bf
Wonwoo Choi <wonwoo9762@berkeley.edu>
2022-10-03 23:49:57 +0000

    part 1 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -7,7 +7,28 @@ class MoviesController < ApplicationController
     end
   
     def index
-      @movies = Movie.all
+      puts "hello world"
+      puts params
+      
+      @all_ratings =  Movie.all_ratings
+      @param_ratings =  params[:ratings].nil? ? {} : params[:ratings]
+      @ratings_to_show = params[:ratings].nil? ?  @all_ratings : params[:ratings].keys ;
+      puts @all_ratings
+      puts @param_ratings
+      puts @ratings_to_show
+      puts "bye world"
+  
+      @sort = params[:sort].nil? ? "" : params[:sort]    
+      unless (params[:sort].present? && params[:ratings].present?)
+        h = {}.compare_by_identity
+        @all_ratings.each_with_index{|k,v| h[k] = v} 
+        redirect_to movies_path(sort: session[:sort] || "id" , ratings: session[:ratings] || h)
+        return
+      end
+      session[:ratings] =  params[:ratings]
+      session[:sort] = params[:sort]
+      
+      @movies = Movie.with_ratings(@ratings_to_show).order(@sort)
     end
   
     def new
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,18 @@
 class Movie < ActiveRecord::Base
+  
+  def Movie.all_ratings
+    ['G','PG','PG-13','R']
+  end
+
+  def Movie.with_ratings(ratings_to_show)
+    display_rating1 = []
+    ratings_to_show.each_with_index  do |val, index| 
+      display_rating1[index] = val
+    end
+    unless display_rating1.length == 0
+      where(rating:display_rating1)
+    else
+      where("")
+    end
+  end
 end
